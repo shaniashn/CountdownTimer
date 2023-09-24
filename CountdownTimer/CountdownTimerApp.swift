e746d0324884328d710899bf6d65f1d73e1a4704
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-09-24 13:25:49 +0700

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Sha Nia Siahaan on 24/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
