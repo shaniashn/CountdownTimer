d278e109a9d572a6494a72714c5363565731c520
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-09-26 00:37:37 +0700

    add model

---
+++ CountdownTimer/CountDownModel.swift
@@ -0,0 +1,14 @@
+//
+//  CountDownModel.swift
+//  CountdownTimer
+//
+//  Created by Sha Nia Siahaan on 26/09/23.
+//
+
+import Foundation
+
+struct TheEvents: Identifiable {
+    var id: UUID = UUID()
+    var title: String
+    var date: Date
+}
