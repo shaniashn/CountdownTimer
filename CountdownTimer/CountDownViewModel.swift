d2551b865597607b59992c6dd835ca5c95750b73
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-09-28 21:39:11 +0700

    add divider and rows

--- CountdownTimer/CountDownViewModel.swift
+++ CountdownTimer/CountDownViewModel.swift
@@ -11,7 +11,26 @@ class CountDownViewModel: ObservableObject {
     @Published var event: [TheEvents] = [] //tambah array untuk collecting data
     
     func addEvents(title: String, date: Date) { //tujuannya adalah memasukkan 1 TheEvents (object) -> [TheEvents(), TheEvents()]
-        var newEvent: TheEvents = TheEvents(title: title, date: date)
+        let newEvent: TheEvents = TheEvents(title: title, date: date)
         event.append(newEvent)
     }
+    
+    func deleteEvents() {
+        
+    }
+    
+    func getDate(date: Date) {
+        let date = Calendar.current.dateComponents([ .year, .month, .weekOfMonth, .weekOfYear, .day], from: date, to: Date())
+        let year = date.year ?? 0
+        var month = date.month ?? 0
+        var weekOfMonth = date.weekOfMonth ?? 0
+        var weekOfYear = date.weekOfYear ?? 0
+        var day = date.day ?? 0
+        print(date)
+        
+//        var formatter = DateFormatter()
+//        formatter.dateFormat = " "
+        
+    }
+
 }
