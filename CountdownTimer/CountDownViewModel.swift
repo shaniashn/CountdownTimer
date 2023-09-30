b5d67218f7fc3fbf220eaf3e25f02938f9d59a6e
Sha Nia Siahaan <shaniashn76@gmail.com>
2023-09-30 17:51:09 +0700

    fix warnings

--- CountdownTimer/CountDownViewModel.swift
+++ CountdownTimer/CountDownViewModel.swift
@@ -11,7 +11,7 @@ class CountDownViewModel: ObservableObject {
     @Published var event: [TheEvents] = [] //tambah array untuk collecting data
     
     func addEvents(title: String, date: Date) { //tujuannya adalah memasukkan 1 TheEvents (object) -> [TheEvents(), TheEvents()]
-        var remainings = getDate(date: date)
+        let remainings = getDate(date: date)
         let newEvent: TheEvents = TheEvents(title: title, date: date, remaining: remainings)
         event.append(newEvent)
     }
@@ -26,7 +26,7 @@ class CountDownViewModel: ObservableObject {
 //        var month = date.month ?? 0
 //        var weekOfMonth = date.weekOfMonth ?? 0
 //        var weekOfYear = date.weekOfYear ?? 0
-        var day = date.day ?? 0
+        let day = date.day ?? 0
         
 //        var formatter = DateFormatter()
 //        formatter.dateFormat = " "
