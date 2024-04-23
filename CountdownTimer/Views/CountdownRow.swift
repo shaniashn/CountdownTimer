58eb698460710c5524ddecdf81df250a62638e68
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-23 15:56:32 +0700

    add text for time left and delete unused logic

--- CountdownTimer/Views/CountdownRow.swift
+++ CountdownTimer/Views/CountdownRow.swift
@@ -8,16 +8,7 @@
 import SwiftUI
 
 struct CountdownRow: View {
-//    @ObservedObject var viewModel: CountDownViewModel
-    
-//    @State var titleEvent: String
-//    @State var dateEvent: Date
-//    @State var dateEvent: DateComponents
-//    @State var dateEvent: Int
-//    @State var dateEvent: [Int]
-    
     var event: Event 
-    
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
@@ -27,31 +18,26 @@ struct CountdownRow: View {
                     .font(.system(size: 20, weight: .medium))
                     .fontWeight(.semibold)
                 
-                HStack {
-//                    if dateEvent.count == 1 {
-//                        Text("\(dateEvent[0]) second")
-//                            .font(.system(size: 14, weight: .regular))
-//                    } else if dateEvent.count == 2 {
-//                        Text("\(dateEvent[0]) second \(dateEvent[1]) hour")
-//                            .font(.system(size: 14, weight: .regular))
-//                    } else if dateEvent.count == 3 {
-//                        Text("\(dateEvent[0]) second \(dateEvent[1]) hour \(dateEvent[2]) day")
-//                            .font(.system(size: 14, weight: .regular))
-//                    }
+              /*  HStack {
+                    if dateEvent.count == 1 {
+                        Text("\(dateEvent[0]) second")
+                            .font(.system(size: 14, weight: .regular))
+                    } else if dateEvent.count == 2 {
+                        Text("\(dateEvent[0]) second \(dateEvent[1]) hour")
+                            .font(.system(size: 14, weight: .regular))
+                    } else if dateEvent.count == 3 {
+                        Text("\(dateEvent[0]) second \(dateEvent[1]) hour \(dateEvent[2]) day")
+                            .font(.system(size: 14, weight: .regular))
+                    }
             
                     
-//                    ForEach(dateEvent, id: \.self) { components in
-//                        Text("\(components)")
-                        /*
-                         jika komponennya 1 berarti hour
-                         jika komponennya 2 berarti hour day
-                         jika komponennya 3 berarti hour day month
-                         dst.
-                         */
-//                    }
+                    ForEach(dateEvent, id: \.self) { components in
+                        Text("\(components)")
+                    }
+                } */
+                HStack {
+                    Text(event.timeLeft)
                 }
-//                .onReceive(timer) { _ in
-//                }
             }
             Spacer()
             Image(systemName: "chevron.forward")
