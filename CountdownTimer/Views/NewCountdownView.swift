7193a2227f7dfc4f04f0f744ab8b65eb3aeda100
Sha Nia Siahaan <shaniashn76@gmail.com>
2024-04-23 15:57:00 +0700

    add alert (not finish yet)

--- CountdownTimer/Views/NewCountdownView.swift
+++ CountdownTimer/Views/NewCountdownView.swift
@@ -10,21 +10,12 @@ import SwiftUI
 //var globalDate: Date = Date()
 
 struct NewCountdownView: View {
-//    @ObservedObject var viewModel: CountDownViewModel
     @EnvironmentObject var vm: CountDownViewModel
-    
-    @Environment(\.dismiss) var isPresented
-    
+    @State var isPresented: Bool = false
     @State var selectedDate: Date = Date()
-    
     @State var eventTitle: String = ""
-//    var calend = Calendar.current.
-
-    
     @FocusState var focusedText: Bool
-    
-    
-    
+
     var body: some View {
         NavigationStack {
             VStack(spacing: 20){
@@ -43,21 +34,16 @@ struct NewCountdownView: View {
             .padding()
             .toolbar {
                 ToolbarItem(placement: .confirmationAction){
-                    Button("Save"){
-                            //                        viewModel.addEventss(title: eventTitle, date: selectedDate)
-                            //                        globalDate = selectedDate
-                            //                        isPresented.callAsFunction()
-                            //                        vm.
-                        
-                            //                        selectedDate.description(with: .autoupdatingCurrent)
-                            //                        print(format.string(from: selectedDate))
-                        
-//                        vm.addEvent(title: eventTitle, date: selectedDate)
-                        remainingTime(date: selectedDate)
-                    }
+                    Button(action: saveEvent, label: {
+                        Text("Save")
+                    })
                 }
             }
+            .alert("Event must be more than 3 character.", isPresented: $isPresented) {
+                Text("OK")
+            }
         }
+        
         
     }
     
@@ -85,73 +71,92 @@ struct NewCountdownView: View {
 //        
 //        var calend = Calendar.current.component(.minute, from: selectedDate)
 //        var calenComp = Calendar.current.dateComponents(in: .autoupdatingCurrent, from: selectedDate)
-//        var cskd = Calendar.current.dateComponents([.hour, .minute], from: selectedDate)
+//        var componentDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: selectedDate)
 //        
 //        var dateComp = DateComponents(calendar: calenComp.calendar, timeZone: TimeZone(identifier: "Asia/Jakarta"), year: calenComp.year, month: calenComp.month, day: calenComp.day, hour: calenComp.hour, minute: calenComp.minute, second: calenComp.second)
 //        
 //        var compToCalend = Calendar(identifier: .gregorian).date(from: selectedDateComp) ?? Date()
 //        var woIdent = Calendar.current.date(from: selectedDateComp) ?? Date()
 //
-//        var selectedDateComp0 = Calendar.current.date(byAdding: /*selectedDateComp*/ cskd, to: selectedDate) ?? Date()
+//        var selectedDateComp0 = Calendar.current.date(byAdding: componentDate, to: selectedDate) ?? Date()
 //        
-//        print("selectedDate -> ", selectedDate)
-//        print("component -> ", compToCalend)
-//        print("current component -> ", woIdent)
-//        print("selectedDate desc -> ", selectedDate.description(with: .autoupdatingCurrent))
-//        print("compToCalend desc -> ", compToCalend.description(with: .autoupdatingCurrent))
-//        print("woIdent desc -> ", woIdent.description(with: .autoupdatingCurrent))
+//        
+////        print("component -> ", compToCalend)
+////        print("current component -> ", woIdent)
+////        print("selectedDate -> ", selectedDate)
+////        print("selectedDate desc -> ", selectedDate.description(with: .autoupdatingCurrent))
+////        print("compToCalend desc -> ", compToCalend.description(with: .autoupdatingCurrent))
+////        print("woIdent desc -> ", woIdent.description(with: .autoupdatingCurrent))
+//        
+//        print("componentDate -> ", componentDate)
 //        
 //        
 ////        return changedDate // string
 //        return compToCalend
 //    }
     
-    func remainingTime(date: Date) -> String {
-        let dateRemaining = Calendar.current.dateComponents([.day,.month,.year,.hour], from: Date(), to: date)
-        
-        var year = dateRemaining.year ?? 0
-        var month = dateRemaining.month ?? 0
-        var day = dateRemaining.day ?? 0
-        var hour = dateRemaining.hour ?? 0
-        
-        var remaining: [Int] = [year, month, day, hour]
-//        var remain: [String : Int] = [:]
-//        if notZeroChecker(component: year) {
+//    func remainingTime(date: Date) -> String {
+//        let dateRemaining = Calendar.current.dateComponents([.day,.month,.year,.hour], from: Date(), to: date)
+//        
+//        var year = dateRemaining.year ?? 0
+//        var month = dateRemaining.month ?? 0
+//        var day = dateRemaining.day ?? 0
+//        var hour = dateRemaining.hour ?? 0
+//        
+//        var remaining: [Int] = [year, month, day, hour]
+////        var remaining: [Int] = [hour, day, month, year]
+//        
+//        func notZeroChecker(remain: [Int]) -> [Int]{
+//            print(remaining)
+//            for rem in 0..<remaining.count {
+//                if remaining[0] == 0 {
+//                    remaining.remove(at: 0)
+//                    print("remaining ", remaining)
+//                }
+//            }
 //            
+//            return remaining
 //        }
-        
-        func notZeroChecker(remain: [Int]) -> [Int]{
-            print(remaining)
-            for rem in remaining {
-                if remaining[rem] == 0 {
-                    remaining.remove(at: rem)
-                    print(remaining)
-                }
-            }
-            
-            return remain
+//        
+//        var timeCount = notZeroChecker(remain: remaining)
+//        print("timeCount ", timeCount)
+//        
+//        var texts = ["\(hour) hour ", "\(day) day ", "\(month) month ", "\(year) year "]
+////        var texts = ["\(year) year ", "\(month) month ", "\(day) day ", "\(month) month ", "\(hour) hour "]
+//        
+//        
+//        var textRemaining: String = ""
+//        
+//        for i in 0..<timeCount.count {
+//            print("timeCount count ", timeCount.count)
+//            textRemaining += texts[i]
+//        }
+//        
+////        switch(timeCount.count){
+////        case 1:
+////            textRemaining = "\(hour) hour"
+////        case 2:
+////            textRemaining = "\(day) day \(hour) hour"
+////        default:
+////            textRemaining = ""
+////        }
+//        print(textRemaining)
+//        return textRemaining
+//    }
+    
+    func saveEvent(){
+        if checkAppropriate() {
+            vm.addEvent(title: eventTitle, date: selectedDate)
+        }
+    }
+    
+    func checkAppropriate() -> Bool{
+        if eventTitle.count < 3 {
+            isPresented = true
+            return false
         }
         
-        var timeCount = notZeroChecker(remain: remaining)
-        
-        var texts = ["\(day) day", "\(hour) hour"]
-        
-        for i in 0..<timeCount.count {
-            
-        }
-        
-        var textRemaining: String = ""
-        
-        switch(timeCount.count){
-        case 1:
-            textRemaining = "\(hour) hour"
-        case 2:
-            textRemaining = "\(day) day \(hour) hour"
-        default:
-            textRemaining = ""
-        }
-        
-        return textRemaining
+        return true
     }
     
 //    func notZeroChecker(component: Int) -> Bool {
